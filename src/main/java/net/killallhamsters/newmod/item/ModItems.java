dba819c69c8d5114e1626e6446486983a77dd13f
dolemitesampson <101364381+dolemitesampson@users.noreply.github.com>
2022-07-05 03:24:29 -0500

    Added Tin Block variants

--- src/main/java/net/killallhamsters/newmod/item/ModItems.java
+++ src/main/java/net/killallhamsters/newmod/item/ModItems.java
@@ -3,17 +3,16 @@ package net.killallhamsters.newmod.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.killallhamsters.newmod.HoneyWine;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
     public static final Item RAW_TIN = registerItem("raw_tin",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.TIN)));
 
     public static final Item TIN = registerItem("tin",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.TIN)));
 
 
     private static Item registerItem(String name, Item item){
