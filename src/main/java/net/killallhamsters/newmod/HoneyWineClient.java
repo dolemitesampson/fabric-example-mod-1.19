527ad830979dd967be8eff9467f63b07e424b26a
dolemitesampson <101364381+dolemitesampson@users.noreply.github.com>
2022-07-05 01:50:39 -0500

    Initial commit

---
+++ src/main/java/net/killallhamsters/newmod/HoneyWineClient.java
@@ -0,0 +1,10 @@
+package net.killallhamsters.newmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HoneyWineClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
