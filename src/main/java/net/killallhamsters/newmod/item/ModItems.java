cd5a77ed17f0deada1deca392bba21a3cba5b6a4
dolemitesampson <101364381+dolemitesampson@users.noreply.github.com>
2022-07-05 02:36:13 -0500

    Added raw tin

---
+++ src/main/java/net/killallhamsters/newmod/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.killallhamsters.newmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.killallhamsters.newmod.HoneyWine;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+
+    public static final Item RAW_TIN = registerItem("raw_tin",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item TIN = registerItem("tin",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registry.ITEM, new Identifier(HoneyWine.MOD_ID, name), item);
+    }
+    public static void registerModItems()  {
+        HoneyWine.LOGGER.debug("Registering Mod Items for" + HoneyWine.MOD_ID);
+    }
+}
