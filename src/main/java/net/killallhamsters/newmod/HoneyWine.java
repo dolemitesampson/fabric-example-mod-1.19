f513b7b4d2467f175ffbd979660146615da4eed6
dolemitesampson <101364381+dolemitesampson@users.noreply.github.com>
2022-07-05 02:47:04 -0500

    Added Tin Group

--- src/main/java/net/killallhamsters/newmod/HoneyWine.java
+++ src/main/java/net/killallhamsters/newmod/HoneyWine.java
@@ -1,15 +1,18 @@
 package net.killallhamsters.newmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.killallhamsters.newmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HoneyWine implements ModInitializer {
-    public static final String MOD_ID = "honeywine";
+    public static final String MOD_ID = "newmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
+
+        ModItems.registerModItems();
 
 
     }
